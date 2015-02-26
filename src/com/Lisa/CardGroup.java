0cf95d97e7b3b654521a901a133e14ba65ae24b8
LisaMabley <lisamabley@gmail.com>
2015-02-26 17:03:19 -0600

    Adding CardGroup class.

---
+++ src/com/Lisa/CardGroup.java
@@ -0,0 +1,18 @@
+package com.Lisa;
+
+// Created by lisa on 2/26/15.
+
+import java.util.LinkedList;
+
+public class CardGroup {
+
+    protected LinkedList<Card> group = new LinkedList<Card>();
+
+    public LinkedList<Card> getGroup() {
+        return group;
+    }
+
+    public void addCard(Card card) {
+        this.group.add(card);
+    }
+}
